0b137034e361fbdb5669b2ebb845949ced0faf38
Brian Arpie <brian@crisply.com>
2014-09-24 19:35:50 -0400

    fuck directives

--- assets/javascripts/directives/countdownDirective.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('CountdownApp');
-
-app.directive('countdown', function() {
-  return {
-    restrict: 'E',
-    controller: 'TickTockController',
-    templateUrl: 'assets/pages/_countdown.html'
-  }
-});
